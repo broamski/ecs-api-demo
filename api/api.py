1f5876c699fd380a3903a29a4faa7a225033ad28
broamski <brian.nuszkowski@me.com>
2016-09-16 14:07:29 -0400

    bump version to 1.3

--- api/api.py
+++ api/api.py
@@ -5,6 +5,7 @@ from flask.ext.redis import FlaskRedis
 
 app = Flask(__name__)
 app.config['REDIS_URL'] = "redis://redis:6379"
+app.config['APP_VERSION'] = "1.3"
 
 redis_store = FlaskRedis(app)
 
@@ -14,13 +15,14 @@ def status():
     print vars(request)
     return jsonify(status="OKAY", requestor_ip=request.remote_addr,
                    user_agent=request.headers.get('User-Agent', 'none'),
-                   timestamp=datetime.datetime.utcnow(), api_version="1.2")
+                   timestamp=datetime.datetime.utcnow(),
+                   api_version=app.config['APP_VERSION'])
 
 
 @app.route("/incrementer")
 def incrementer():
     counter = redis_store.incr('incrementer')
-    return jsonify(incrementer=counter)
+    return jsonify(incrementer=counter, api_version=app.config['APP_VERSION'])
 
 
 if __name__ == "__main__":
